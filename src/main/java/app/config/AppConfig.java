baab0b7b32effe3e135f03d8c35bd94ee40be48c
Sergey Tsoy <hekonnote@gmail.com>
2020-10-29 20:28:09 +0600

    done2

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,5 +1,6 @@
 package app.config;
 
+import app.model.Cat;
 import app.model.Dog;
 import app.model.Timer;
 import org.springframework.context.annotation.Bean;
@@ -10,13 +11,18 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "app")
 public class AppConfig {
 
-    @Bean
-    public Dog getDog() {
-        return new Dog();
-    }
-
-    @Bean
-    public Timer getTimer() {
-        return new Timer();
-    }
+//    @Bean
+//    public Dog getDog() {
+//        return new Dog();
+//    }
+//
+//    @Bean
+//    public Cat getCat() {
+//        return new Cat();
+//    }
+//
+//    @Bean
+//    public Timer getTimer() {
+//        return new Timer();
+//    }
 }
